f1ee155923b8278cc38a702f967b8d98680276b3
AngeloArtur <ang.vianna@gmail.com>
2025-01-09 19:28:02 -0300

    feat: rotas

--- src/Routes/MainRoutes.jsx
+++ src/Routes/MainRoutes.jsx
@@ -1,12 +1,14 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom"
+import Login from "../pages/Login/Login"
+import FortgotPassword from "../pages/Forgot-Password/ForgotPassword"
 
 export default function MainRoutes() {
-    return (
-        <div>
-            <Routes>
-                <Route></Route>
-            </Routes>
-        </div>
-    );
+  return (
+    <div>
+      <Routes>
+        <Route path="/login" element={Login}></Route>
+        <Route path="/forgot-password" element={FortgotPassword}></Route>
+      </Routes>
+    </div>
+  )
 }
