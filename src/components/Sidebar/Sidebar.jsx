bd60f698477e6fef89c520198ce2749b2e8cc6c8
Ângelo Artur <ang.vianna@gmail.com>
2025-01-20 17:28:47 -0300

    feat: renderizando itens

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,190 +1,172 @@
-import * as React from "react"
-import { styled } from "@mui/material/styles"
-import Box from "@mui/material/Box"
-import MuiDrawer from "@mui/material/Drawer"
-import List from "@mui/material/List"
-import Divider from "@mui/material/Divider"
-import IconButton from "@mui/material/IconButton"
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import MuiDrawer from "@mui/material/Drawer";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
 
-import ListItem from "@mui/material/ListItem"
-import ListItemButton from "@mui/material/ListItemButton"
-import ListItemIcon from "@mui/material/ListItemIcon"
-import ListItemText from "@mui/material/ListItemText"
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
 
 import {
-  MdMenuOpen,
-  MdOutlineMenu,
-  MdOutlineClose,
-  MdPersonPin,
-  MdDashboard,
-  MdSettings,
-  MdExitToApp,
-  MdAllInbox,
-  MdOutlineMail,
-} from "react-icons/md"
+    MdMenuOpen,
+    MdOutlineMenu,
+    MdPersonPin,
+    MdDashboard,
+    MdSettings,
+    MdExitToApp,
+} from "react-icons/md";
 
 const NAVIGATION = [
-  {
-    segment: "dashboard",
-    title: "Dashboard",
-    icon: <MdDashboard />,
-  },
-  {
-    segment: "settings",
-    title: "Settings",
-    icon: <MdSettings />,
-  },
-  {
-    kind: "divider",
-  },
+    {
+        segment: "dashboard",
+        title: "Dashboard",
+        icon: <MdDashboard />,
+    },
+    {
+        segment: "settings",
+        title: "Settings",
+        icon: <MdSettings />,
+    },
+    {
+        kind: "divider",
+    },
 
-  {
-    segment: "reports",
-    title: "Reports",
-    icon: <MdPersonPin />,
-  },
-  {
-    segment: "reports",
-    title: "Exit App",
-    icon: <MdExitToApp />,
-  },
-]
+    {
+        segment: "reports",
+        title: "Reports",
+        icon: <MdPersonPin />,
+    },
+    {
+        segment: "reports",
+        title: "Exit App",
+        icon: <MdExitToApp />,
+    },
+];
 
-const drawerWidth = 240
+export const drawerWidth = 240;
 
 const openedMixin = (theme) => ({
-  width: drawerWidth,
-  transition: theme.transitions.create("width", {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.enteringScreen,
-  }),
-  overflowX: "hidden",
-})
+    width: drawerWidth,
+    transition: theme.transitions.create("width", {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.enteringScreen,
+    }),
+    overflowX: "hidden",
+});
 
 const closedMixin = (theme) => ({
-  transition: theme.transitions.create("width", {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  overflowX: "hidden",
-  width: `calc(${theme.spacing(7)} + 1px)`,
-  [theme.breakpoints.up("sm")]: {
-    width: `calc(${theme.spacing(8)} + 1px)`,
-  },
-})
+    transition: theme.transitions.create("width", {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+    }),
+    overflowX: "hidden",
+    width: `calc(${theme.spacing(7)} + 1px)`,
+    [theme.breakpoints.up("sm")]: {
+        width: `calc(${theme.spacing(8)} + 1px)`,
+    },
+});
 
 const DrawerHeader = styled("div")(({ theme }) => ({
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "flex-end",
-  padding: theme.spacing(0, 1),
-  ...theme.mixins.toolbar,
-}))
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-end",
+    padding: theme.spacing(0, 1),
+    ...theme.mixins.toolbar,
+}));
 
 const Drawer = styled(MuiDrawer, {
-  shouldForwardProp: (prop) => prop !== "open",
+    shouldForwardProp: (prop) => prop !== "open",
 })(({ theme, open }) => ({
-  width: drawerWidth,
-  flexShrink: 0,
-  whiteSpace: "nowrap",
-  boxSizing: "border-box",
-  ...(open && {
-    ...openedMixin(theme),
-    "& .MuiDrawer-paper": openedMixin(theme),
-  }),
-  ...(!open && {
-    ...closedMixin(theme),
-    "& .MuiDrawer-paper": closedMixin(theme),
-  }),
-}))
+    width: drawerWidth,
+    flexShrink: 0,
+    whiteSpace: "nowrap",
+    boxSizing: "border-box",
+    ...(open && {
+        ...openedMixin(theme),
+        "& .MuiDrawer-paper": openedMixin(theme),
+    }),
+    ...(!open && {
+        ...closedMixin(theme),
+        "& .MuiDrawer-paper": closedMixin(theme),
+    }),
+}));
 
-export default function Sidebar() {
-  const [open, setOpen] = React.useState(false)
+export default function Sidebar({ open, handleDrawerOpen }) {
+    return (
+        <Box sx={{ display: "flex" }}>
+            <Drawer variant="permanent" open={open} className="!pr-5">
+                <DrawerHeader>
+                    <IconButton
+                        color="inherit"
+                        aria-label={open ? "close drawer" : "open drawer"}
+                        onClick={handleDrawerOpen}
+                        edge="start"
+                        sx={{ color: "black" }}>
+                        {open ? <MdMenuOpen /> : <MdOutlineMenu />}
+                    </IconButton>
+                </DrawerHeader>
 
-  const handleDrawerOpen = () => {
-    setOpen(!open)
-  }
+                <Divider />
 
-  return (
-    <Box sx={{ display: "flex" }}>
-      <Drawer variant="permanent" open={open}>
-        <DrawerHeader>
-          <IconButton
-            color="inherit"
-            aria-label={open ? "close drawer" : "open drawer"}
-            onClick={handleDrawerOpen}
-            edge="start"
-            sx={{ color: "black" }}
-          >
-            {open ? <MdMenuOpen /> : <MdOutlineMenu />}
-          </IconButton>
-        </DrawerHeader>
-
-        <Divider />
-
-        <List>
-          {NAVIGATION.map((item, index) => {
-            if (item.kind === "divider") {
-              return <Divider key={index} />
-            }
-
-            // Renderiza itens normais de navegação
-            return (
-              <ListItem
-                key={item.segment}
-                disablePadding
-                sx={{ display: "block" }}
-              >
-                <ListItemButton
-                  sx={[
-                    {
-                      minHeight: 48,
-                      px: 3,
-                    },
-                    open
-                      ? {
-                          justifyContent: "initial",
+                <List>
+                    {NAVIGATION.map((item, index) => {
+                        if (item.kind === "divider") {
+                            return <Divider key={index} />;
                         }
-                      : {
-                          justifyContent: "center",
-                        },
-                  ]}
-                >
-                  <ListItemIcon
-                    sx={[
-                      {
-                        minWidth: 0,
-                        justifyContent: "center",
-                      },
-                      open
-                        ? {
-                            mr: 3,
-                          }
-                        : {
-                            mr: "auto",
-                          },
-                    ]}
-                  >
-                    {item.icon}
-                  </ListItemIcon>
-                  <ListItemText
-                    primary={item.title}
-                    sx={[
-                      open
-                        ? {
-                            opacity: 1,
-                          }
-                        : {
-                            opacity: 0,
-                          },
-                    ]}
-                  />
-                </ListItemButton>
-              </ListItem>
-            )
-          })}
-        </List>
-      </Drawer>
-    </Box>
-  )
+
+                        return (
+                            <ListItem key={item.segment} disablePadding sx={{ display: "block" }}>
+                                <ListItemButton
+                                    sx={[
+                                        {
+                                            minHeight: 48,
+                                            px: 3,
+                                        },
+                                        open
+                                            ? {
+                                                  justifyContent: "initial",
+                                              }
+                                            : {
+                                                  justifyContent: "center",
+                                              },
+                                    ]}>
+                                    <ListItemIcon
+                                        sx={[
+                                            {
+                                                minWidth: 0,
+                                                justifyContent: "center",
+                                            },
+                                            open
+                                                ? {
+                                                      mr: 3,
+                                                  }
+                                                : {
+                                                      mr: "auto",
+                                                  },
+                                        ]}>
+                                        {item.icon}
+                                    </ListItemIcon>
+                                    <ListItemText
+                                        primary={item.title}
+                                        sx={[
+                                            open
+                                                ? {
+                                                      opacity: 1,
+                                                  }
+                                                : {
+                                                      opacity: 0,
+                                                  },
+                                        ]}
+                                    />
+                                </ListItemButton>
+                            </ListItem>
+                        );
+                    })}
+                </List>
+            </Drawer>
+        </Box>
+    );
 }
