1a7079d54cccdc4984589177587dbf7c119dfc04
Ângelo Artur <ang.vianna@gmail.com>
2025-01-20 17:26:52 -0300

    fix: correção de rotas

--- src/Routes/MainRoutes.jsx
+++ src/Routes/MainRoutes.jsx
@@ -1,14 +1,16 @@
-import { Route, Routes } from "react-router-dom"
-import Login from "../pages/Login/Login"
-import FortgotPassword from "../pages/Forgot-Password/ForgotPassword"
+import { Route, Routes } from "react-router-dom";
+import Login from "../pages/Login/Login";
+import FortgotPassword from "../pages/Forgot-Password/ForgotPassword";
+import Home from "../pages/Home/home";
+import AlunoDashboard from "../pages/AlunoDashboard/AlunoDashboard";
 
 export default function MainRoutes() {
-  return (
-    <div>
-      <Routes>
-        <Route path="/login" element={Login}></Route>
-        <Route path="/forgot-password" element={FortgotPassword}></Route>
-      </Routes>
-    </div>
-  )
+    return (
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/forgot-password" element={<FortgotPassword />} />
+            <Route path="/dashboard" element={<AlunoDashboard />} />
+        </Routes>
+    );
 }
