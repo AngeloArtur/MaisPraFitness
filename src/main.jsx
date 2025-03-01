39670fcf8e2be232bdeea5341b920ec8576ffaeb
Ângelo Artur <ang.vianna@gmail.com>
2025-03-01 16:58:01 -0300

    ajuste de rotas e de autenticação

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,16 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { BrowserRouter, useLocation } from "react-router-dom";
 import Sidebar, { drawerWidth } from "./components/Sidebar/Sidebar";
-import MainRoutes from "./Routes/MainRoutes.jsx";
 import { useTheme } from "@mui/material/styles";
+import { AuthProvider } from "./context/AuthContext";
+import Routes from "./Routes/index.jsx";
+import { useAuth } from "./context/AuthContext.jsx";
 
 const closedWidth = (theme) => `calc(${theme.spacing(7)} + 1px)`;
 
 const App = () => {
     const location = useLocation();
+    const { isAuthenticated } = useAuth();
     const hideSidebarRoutes = ["/login", "/forgot-password"];
     const shouldRenderSidebar = !hideSidebarRoutes.includes(location.pathname);
     const [open, setOpen] = useState(false);
@@ -20,15 +23,15 @@ const App = () => {
     };
     return (
         <>
-            {shouldRenderSidebar && <Sidebar open={open} handleDrawerOpen={handleDrawerOpen} />}
+            {(shouldRenderSidebar && isAuthenticated) && <Sidebar open={open} handleDrawerOpen={handleDrawerOpen} />}
             <main
                 style={{
-                    ...(shouldRenderSidebar && {
+                    ...((shouldRenderSidebar && isAuthenticated) && {
                         marginLeft: open ? `${drawerWidth}px` : closedWidth(theme),
                         transition: "margin 0.3s ease",
                     }),
                 }}>
-                <MainRoutes />
+                <Routes />
             </main>
         </>
     );
@@ -36,6 +39,8 @@ const App = () => {
 
 createRoot(document.getElementById("root")).render(
     <BrowserRouter>
-        <App />
+        <AuthProvider>
+            <App />
+        </AuthProvider>
     </BrowserRouter>
 );
